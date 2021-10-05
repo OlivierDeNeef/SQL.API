b73c921da94c3f9d5da89c9248d00fba3e943553
Olivier De Neef <71842536+Oli4dn@users.noreply.github.com>
2021-10-06 00:10:13 +0200

    Update

--- SQL/DatabaseAdapter.cs
+++ SQL/DatabaseAdapter.cs
@@ -129,7 +129,7 @@ namespace SQL
         /// Manipulate data from a database (Insert/Update/Delete/..) en returns one value via OUTPUT clause.
         /// </summary>
         /// <param name="sqlQuery">Query with syntax: "INSERT INTO MyTable OUTPUT INSERTED.[columnName] VALUES (MyValue1,MyValue2,..  )"</param>
-        /// <returns>One value of the alterded row</returns>
+        /// <returns>A value of the altered row</returns>
         public object ChangeAndGetValue(string sqlQuery)
         {
             if (string.IsNullOrWhiteSpace(ConnectionString))
@@ -155,56 +155,7 @@ namespace SQL
 
 
 
-        /// <summary>
-        /// This execute queries the database via a transaction so that all queries are always executed or rolled back to current state
-        /// </summary>
-        /// <param name="sqlQuerys">A list of queys.</param>
-        /// <param name="transactionName">The name of the coming transaction.</param>
-        /// <returns>A count of all affected rows.</returns>
-        public int Transaction(IEnumerable<string> sqlQuerys, string transactionName)
-        {
-            if (string.IsNullOrWhiteSpace(ConnectionString))
-                throw new NullReferenceException(
-                    "The connection could not be established because connection string was empty");
-
-            var affectedRows = 0;
-
-            SqlTransaction transaction = null;
-
-            try
-            {
-                using var con = new SqlConnection(ConnectionString);
-                con.Open();
-                transaction = con.BeginTransaction(transactionName);
-
-                var cmd = new SqlCommand
-                {
-                    Transaction = transaction,
-                    Connection = con
-                };
-
-                foreach (var query in sqlQuerys)
-                {
-                    cmd.CommandText = query;
-                    affectedRows += cmd.ExecuteNonQuery();
-                }
-            }
-            catch (Exception e)
-            {
-                try
-                {
-                    transaction?.Rollback();
-                    throw new DataException("The Transaction could not be established", e);
-                }
-                catch (Exception exception)
-                {
-                    throw new DataException("The Transaction could not be established and a rollback has failed!",
-                        exception);
-                }
-            }
-
-            transaction.Commit();
-            return affectedRows;
-        }
+       
+      
     }
 }
